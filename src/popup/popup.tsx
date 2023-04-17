a542752c434192c76714aa98012ab65ad7e93752
KIATISAK SOMKHUNNGUEN <6010110044@psu.ac.th>
2023-04-17 20:10:08 +0700

    react dom configuration

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,3 +1,8 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
+const test = <div>hello</div>;
 
-const test = <h1>Hello world</h1>;
+const container = document.createElement("div");
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(test);
