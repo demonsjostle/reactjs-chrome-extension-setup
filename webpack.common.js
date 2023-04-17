29391f401615e65aab1dae7c5fa8a8637a754cae
KIATISAK SOMKHUNNGUEN <6010110044@psu.ac.th>
2023-04-17 22:57:03 +0700

    add background and content script

--- webpack.common.js
+++ webpack.common.js
@@ -2,10 +2,11 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development",
   entry: {
     popup: path.resolve("./src/popup/popup.tsx"),
     options: path.resolve("./src/options/options.tsx"),
+    background: path.resolve("./src/background/background.ts"),
+    contentScript: path.resolve("./src/contentScript/contentScript.ts"),
   },
   module: {
     rules: [
@@ -20,15 +21,13 @@ module.exports = {
       },
     ],
   },
-  devtool: "cheap-module-source-map",
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve("src/static/manifest.json"),
+          from: path.resolve("src/static"),
           to: path.resolve("dist"),
         },
-        { from: path.resolve("src/static/icon.png"), to: path.resolve("dist") },
       ],
     }),
     ...getHtmlPlugins(["popup", "options"]),
